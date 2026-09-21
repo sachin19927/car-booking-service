5cd329f5497352c5f7273fa515c86e78d18d0da2
SACHIN HS <sachin19927@gmail.com>
2026-09-21 14:42:56 +0530

    Scheduling enabled

--- src/main/java/com/motors/velocity/carbookingservice/CarBookingServiceApplication.java
+++ src/main/java/com/motors/velocity/carbookingservice/CarBookingServiceApplication.java
@@ -2,7 +2,9 @@ package com.motors.velocity.carbookingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class CarBookingServiceApplication {
 
